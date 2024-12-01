9225c34d8538b1b749dec7ebeb1b69a5b692360f
gn00py48 <gn00py48@gmail.com>
2024-12-01 22:16:52 +0900

    [feat] landing page

---
+++ frontend/ossp_neverdie_fe/src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    margin: 0 auto;
+    flex-direction: column;
+`;
+
+function LandingPage() {
+
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default LandingPage;
