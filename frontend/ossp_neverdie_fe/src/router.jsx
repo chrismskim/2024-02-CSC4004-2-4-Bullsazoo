7c3c64b602e892c54ddeebab1f7fb1a58a477e65
chris <chrismskim@naver.com>
2024-12-01 14:31:32 +0900

    [feat] AboutPage

--- frontend/ossp_neverdie_fe/src/router.jsx
+++ frontend/ossp_neverdie_fe/src/router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import NotFound from "./pages/NotFound";
+import Aboutpage from "./pages/LandingAbout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <MainPage />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "/AboutPage",
+        element: <Aboutpage />,
         errorElement: <NotFound />,
     }
 ]);
