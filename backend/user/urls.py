ada2b72ed4d161f19d8c7f39d40274f898a8abd8
Shinjaeyong <sjyssb@naver.com>
2024-12-05 00:28:21 +0900

    [feat] 로그인/회원가입 페이지 최신화(작업중)

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,24 +1,10 @@
-from django.urls import path, re_path
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
+from django.urls import path
 from .views import UserSignupView, UserLoginView
 
-# Swagger 설정
-schema_view = get_schema_view(
-    openapi.Info(
-        title="User API",
-        default_version='v1',
-        description="User API for signup and login",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
+    # 회원가입 API URL
     path('signup/', UserSignupView.as_view(), name='user_signup'),
+
+    # 로그인 API URL
     path('login/', UserLoginView.as_view(), name='user_login'),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
