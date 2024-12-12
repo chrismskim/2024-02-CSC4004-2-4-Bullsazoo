51550203ec52d097d7814c7a8adf02cec0da43d9
Shinjaeyong <sjyssb@naver.com>
2024-12-12 17:40:15 +0900

    [feat] 물체인식 기능 구현 테스트 완료

--- backend/analyze/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AnalyzeResult
-
-@admin.register(AnalyzeResult)
-class AnalyzeResultAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image', 'created_at']
-    search_fields = ['image']
--- backend/analyze/models.py
+++ backend/analyze/models.py
@@ -1,24 +1,10 @@
 from django.db import models
 
-class AnalyzeResult(models.Model):
-    image = models.ImageField(upload_to='uploads/')
-    detected_objects = models.JSONField()  # YOLO 분석 결과를 JSON 형식으로 저장
-    created_at = models.DateTimeField(auto_now_add=True)  # 생성 시간 자동 기록
+class DetectedObjects(models.Model):
+    user_id = models.IntegerField()
+    object_name = models.CharField(max_length=100)
 
     class Meta:
-        managed = True
-        db_table = 'analyze_result'  # 실제 PostgreSQL 테이블 이름
-
-class DetectedObject(models.Model):
-    class_id = models.IntegerField()  # 클래스 ID
-    class_name = models.CharField(max_length=100)  # 클래스 이름
-    confidence = models.FloatField()  # 신뢰도
-    box = models.JSONField()  # 바운딩 박스 좌표 (JSON 형식)
-    analyze_result = models.ForeignKey(
-        AnalyzeResult,  related_name='related_detected_objects', on_delete=models.CASCADE
-    )
-
-    class Meta:
-        managed = True
-        db_table = 'analyze_detectedobject'  # 실제 PostgreSQL 테이블 이름
-
+        db_table = 'analyze_detectedobjects'  # PostgreSQL의 테이블 이름
+        managed = False                # Django가 이 테이블을 관리하지 않음
+        unique_together = ('user_id', 'object_name')
